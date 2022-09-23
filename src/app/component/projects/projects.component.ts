083b877e2754544f6ebacc972855329ae67392bc
ajayc07 <ajaychandrasekaran8595@gmail.com>
2022-09-23 07:37:08 +0530

    Adding Asset

--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -11,10 +11,17 @@ export class ProjectsComponent implements OnInit {
     {
       title: 'TeeRex Store',
       desc: 'TeeRexStore is a responsive E-commerce site for selling t-shits. Developed as a solution for coding challenge.',
-      techStack: ['TypeScript', 'HTML', 'SCSS'],
+      techStack: ['Angular 14', 'TypeScript', 'HTML', 'SCSS'],
       gitHubLink: 'https://github.com/ajayc07/teeRexStore',
       live: 'https://ajayc07.github.io/teeRexStore/product'
-    }
+    },
+    {
+      title: 'Color Companion',
+      desc: 'Angular application which allows the user to decide best color combo by trying various color combo on a object.',
+      techStack: ['Angular 7', 'TypeScript', 'HTML', 'SCSS'],
+      gitHubLink: 'https://github.com/ajayc07/colorMatcher',
+      live: 'https://ajayc07.github.io/colorMatcher'
+    },
   ]
 
   constructor() { }
